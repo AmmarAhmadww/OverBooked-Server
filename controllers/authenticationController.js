0fc682ebaa68aa0109041fa4d448b587f3169973
AmmarAhmadww <ammarahmed@punch.agency>
2025-01-17 18:43:35 +0500

    asd

---
+++ controllers/authenticationController.js
@@ -0,0 +1,113 @@
+const md5 = require('md5');
+const User = require("../models/userModel.js");
+
+// GET
+// landing page for Overbooked
+exports.getWelcome = function (req, res) {
+    res.json({ message: "Welcome to Overbooked!" });
+}
+
+// GET
+// login page
+exports.getLogin = function (req, res) {
+    res.json({ message: "Login page", dangerMessage: "true" });
+}
+
+// POST
+// login authentication
+exports.postLogin = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        // Find user by username
+        const user = await User.findOne({ username });
+        
+        // If user doesn't exist
+        if (!user) {
+            return res.status(401).json({
+                success: false,
+                message: "Username not found"
+            });
+        }
+
+        // Check password (assuming password is stored as plain text for now)
+        if (user.password !== password) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid password"
+            });
+        }
+
+        // Successful login
+        res.json({
+            success: true,
+            user: {
+                _id: user._id,
+                username: user.username,
+                isAdmin: user.isAdmin,
+                issuedBooks: user.issuedBooks || []
+            }
+        });
+
+    } catch (err) {
+        console.error("Login error:", err);
+        res.status(500).json({
+            success: false,
+            message: "An error occurred during login"
+        });
+    }
+};
+
+// GET
+// registration page
+exports.getRegister = function (req, res) {
+    res.json({ message: "Registration page", dangerMessage: "true" });
+}
+
+// POST
+// registration page
+exports.postRegister = async (req, res) => {
+    try {
+        const { username, password, isAdmin, adminCode } = req.body;
+
+        // Check if trying to register as admin
+        if (isAdmin) {
+            // Verify against hardcoded admin code
+            if (!adminCode || adminCode !== "4269") {
+                return res.status(403).json({
+                    success: false,
+                    message: "Invalid admin registration code"
+                });
+            }
+        }
+
+        // Check if username already exists
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return res.status(400).json({
+                success: false,
+                message: "Username already exists"
+            });
+        }
+
+        // Create new user
+        const newUser = new User({
+            username,
+            password,
+            isAdmin: isAdmin && adminCode === "4269"
+        });
+
+        await newUser.save();
+
+        res.status(201).json({
+            success: true,
+            message: "Registration successful"
+        });
+    } catch (err) {
+        console.error("Registration error:", err);
+        res.status(500).json({
+            success: false,
+            message: "An error occurred during registration"
+        });
+    }
+};
---
+++ models/userModel.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+
+mongoose.connect(process.env.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('Database connected'))
+  .catch(err => console.log('Error connecting to database:', err));
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  issuedBooks: [
+    {
+      bookName: String,
+      cover: String,
+      pdf: String,
+      bookID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Library'
+      }
+    },
+  ],
+  numberOfIssuedBooks: Number,
+  signedIn: Boolean,
+  isAdmin: {
+    type: Boolean,
+    default: false
+  },
+  readingProgress: {
+    type: Object,
+    default: {}
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
